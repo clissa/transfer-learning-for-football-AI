7eadb4178f5a8fa767c60fb5ec92681e5f3e247f
clissa <clissa.professional@gmail.com>
2026-03-01 15:10:28 +0100

    build(package): add football_ai package exports and pyproject config
    Add package entrypoint via src/football_ai/__init__.py, define setuptools project metadata in pyproject.toml, and ignore build/editor artifacts (.egg-info, dist, build, .vscode).

---
+++ src/football_ai/__init__.py
@@ -0,0 +1,23 @@
+from .utils import (
+    build_and_save_vaep_for_competition_season,
+    build_vaep_dataset_for_competition_season,
+    list_competitions,
+    make_statsbomb_loader,
+    output_paths_for_competition_season,
+    resolve_competition_season_ids,
+    save_vaep_dataset,
+    select_competition_seasons,
+    slug,
+)
+
+__all__ = [
+    "build_and_save_vaep_for_competition_season",
+    "build_vaep_dataset_for_competition_season",
+    "list_competitions",
+    "make_statsbomb_loader",
+    "output_paths_for_competition_season",
+    "resolve_competition_season_ids",
+    "save_vaep_dataset",
+    "select_competition_seasons",
+    "slug",
+]
